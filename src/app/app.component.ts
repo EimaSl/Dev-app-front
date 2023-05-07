53ca3e9cb946fb29eab093158713c98fe01e525c
Linas <l.sarpauskas@gmail.com>
2023-05-07 09:35:41 +0300

    FrontendV1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+export interface Developer {
+
+  id : number;
+  name: string;
+  seniority :string;
+  dob: Date;
+
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'devFront';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  public devs: Developer[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.getDevs();
+  }
+
+  private getDevs(): void {
+    this.getDevelopers().subscribe(
+      (response: Developer[]) => {
+        this.devs = response;
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public getDevelopers(): Observable<Developer[]> {
+    return this.httpClient.get<Developer[]>('http://localhost:1111/api/developer/');
+  }
 }
